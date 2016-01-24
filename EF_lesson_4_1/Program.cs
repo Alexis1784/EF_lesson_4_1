7ca3320d0c5384d82f56681242c697371c2909f8
Alex <alexis1784@gmail.com>
2016-01-24 17:12:24 +0500

    working version

---
+++ EF_lesson_4_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PhoneContext db = new PhoneContext())
+            {
+                var phones = db.Phones.Where(p => p.CompanyId == 1);
+                foreach (Phone ph in phones)
+                {
+                    Console.WriteLine("Name - {0}, Price - {1}, CompanyId - {2}", ph.Name, ph.Price, ph.CompanyId);
+                }
+                var phones2 = from p in db.Phones
+                              where p.CompanyId == 2
+                              select p;
+                foreach (Phone ph in phones2)
+                {
+                    Console.WriteLine("Name - {0}, Price - {1}, CompanyId - {2}", ph.Name, ph.Price, ph.CompanyId);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
